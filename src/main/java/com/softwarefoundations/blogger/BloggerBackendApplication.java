0274800c8f81bd5564b698487d0c4079a30e9205
Dherkyan R. Silva <dherkyan@gmail.com>
2024-08-12 10:37:29 -0300

    Projeto blogger-backend

---
+++ src/main/java/com/softwarefoundations/blogger/BloggerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundations.blogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundations/blogger/BloggerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundations.blogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
